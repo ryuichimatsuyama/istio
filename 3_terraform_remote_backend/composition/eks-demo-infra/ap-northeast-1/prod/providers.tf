5e19b6461c63708bed02535a36c8a5ef69e40cab
root <root@ryuichi.localdomain>
2025-08-10 02:13:14 +0900

    EKS追加

--- 3_terraform_remote_backend/composition/eks-demo-infra/ap-northeast-1/prod/providers.tf
+++ 3_terraform_remote_backend/composition/eks-demo-infra/ap-northeast-1/prod/providers.tf
@@ -2,15 +2,25 @@
 # Provider to connect to AWS
 # https://www.terraform.io/docs/providers/aws/
 ########################################
+provider "aws" {
+  region = var.region
+  # profile = var.profile_name
 
-terraform {
-  required_version = ">= 1.0"
-  backend "s3" {} # use backend.config for remote backend
+  assume_role {
+    role_arn = "arn:aws:iam::${var.account_id}:role/${var.role_name}"
+  }
+}
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.47.0"
-    }
+# In case of not creating the cluster, this will be an incompletely configured, unused provider, which poses no problem.
+# ref: https://github.com/terraform-aws-modules/terraform-aws-eks/blob/v12.1.0/README.md#conditional-creation, https://github.com/terraform-aws-modules/terraform-aws-eks/issues/911
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
 }
